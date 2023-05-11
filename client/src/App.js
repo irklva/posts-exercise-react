bf077a1ae01fece4f60c37dcac48b5a281f86683
irklva <irk.lva@gmail.com>
2023-05-11 17:28:47 +0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {useEffect} from 'react';
 import './App.css';
+import store from './system/store/configureStore';
+import {Provider} from "react-redux";
+import MainComponent from "./components/MainComponent";
 
 function App() {
 
-  useEffect(() => {
-    // TEST API, it might be removed
-    fetch('http://localhost:8080/live').then(res => res.json()).then(res => {
-      console.log('API CONNECTION IS OK');
-    }).catch((e) => console.error('API CONNECTION FAILED, PLEASE CHECK SERVER APP AND TRY AGAIN'))
-  }, []);
+    useEffect(() => {
+        fetch('http://localhost:8080/live').then(res => res.json()).then(res => {
+            console.log('API CONNECTION IS OK');
+        }).catch((e) => console.error('API CONNECTION FAILED, PLEASE CHECK SERVER APP AND TRY AGAIN'))
+    }, []);
 
-  return (
-    <div className="App">
-      TASK IMPLEMENTATION HERE
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <MainComponent/>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
